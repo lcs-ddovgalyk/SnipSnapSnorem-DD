b63c111b94018ddc2755dcd4ff4023df8fe651ca
Joven Peralta <jperalta@lcs.on.ca>
2020-03-05 21:10:58 -0500

    Initial Commit

---
+++ SnipSnapSnorem/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SnipSnapSnorem
+//
+//  Created by Peralta, Joven on 2020-03-05.
+//  Copyright © 2020 Peralta, Joven. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
