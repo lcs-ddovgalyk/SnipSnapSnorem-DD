986b6749aa6480a63c5c08faa5312a5611cf996b
Joven Peralta <jperalta@lcs.on.ca>
2020-03-05 21:23:40 -0500

    Implemented the Structs and Enumerations from the borrowed code.

--- SnipSnapSnorem/Card.swift
+++ SnipSnapSnorem/Card.swift
@@ -1,9 +1,37 @@
 //
 //  Card.swift
-//  SnipSnapSnorem
+//  AlternateCommandLineWar
 //
-//  Created by Peralta, Joven on 2020-03-05.
-//  Copyright © 2020 Peralta, Joven. All rights reserved.
+//  Created by Gordon, Russell on 2020-02-12.
+//  Copyright © 2020 Gordon, Russell. All rights reserved.
 //
 
 import Foundation
+
+// Create a new datatype to represent a playing card
+struct Card {
+
+    // Properties
+    var suit: Suit
+    var rank: Rank
+    
+    // Does the value for this card beat another card?
+    //
+    func beats(_ otherCard: Card) -> Bool {
+        
+        // Use the beats method built into the WarRank type
+        return self.rank.beats(otherCard.rank)
+    }
+    
+    // Report the simple description of this card
+    func simpleDescription() -> String {
+        
+        // Purely text-based description
+        //return self.rank.simpleDescription() + " of " + self.suit.simpleDescription()
+        
+        // Description using the emoji
+        return self.rank.simpleDescription() + " of " + self.suit.glyph
+        
+    }
+    
+}
