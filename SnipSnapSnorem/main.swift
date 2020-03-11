175680dc54abd69a7c213927a1780657cd2a3ba1
Joven Peralta <jperalta@lcs.on.ca>
2020-03-11 19:20:02 -0400

    Set up the variables and initialized them. Also randomly dealt out the cards to the player and computer.
    Closes issue #2.

--- SnipSnapSnorem/main.swift
+++ SnipSnapSnorem/main.swift
@@ -8,5 +8,43 @@
 
 import Foundation
 
-print("Hello, World!")
 
+class SnipSnapSnorem {
+    // Instance variables
+    // The deck of cards
+    var deck : Deck
+    
+    // The hands for each player
+    var player : Hand
+    var computer : Hand
+    
+    // The pile where each player shows their cards
+    var middlePile : Hand
+    
+    // Initializers
+    init () {
+        // Initialize the initial deck (deck that gets shuffled and dealt out to the players)
+        deck = Deck()
+        
+        // Initialize the player hands
+        player = Hand(description: "Player's hand")
+        computer = Hand(description: "Computer's hand")
+        
+        // Initialize the middle pile
+        middlePile = Hand(description: "Middle pile")
+        
+        
+        // Deal to the player
+        if let newCards = self.deck.randomlyDealOut(thisManyCards: 26) {
+            player.cards = newCards
+        }
+        
+        // Deal to the computer
+        if let newCards = self.deck.randomlyDealOut(thisManyCards: 26) {
+            computer.cards = newCards
+        }
+        
+        // Middle pile has no cards to start with
+        middlePile.cards = []
+    }
+}
